8d950ccd4b79ab259ae763d3ba709cb50b396933
Chris Hemp <chris@chrishemp.com>
2025-03-20 08:53:54 -0400

    Update mac packages and firefox script

--- .chezmoiscripts/windows/run_firefox_profile.ps1
+++ .chezmoiscripts/windows/run_firefox_profile.ps1
@@ -1,2 +1,3 @@
+# @sse darwin / linux: .chezmoiscripts/run_firefox_profile.sh.tmpl
 New-Item -ItemType Directory -Force -Path "C:\Program Files\Mozilla Firefox\distribution\"
 Copy-Item ".config\firefox\policies.json" -Destination "C:\Program Files\Mozilla Firefox\distribution\"
