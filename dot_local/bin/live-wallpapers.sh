0ecda1f43677fc610ff1b15dc8312566fabddf1a
Chris Hemp <chris@chrishemp.com>
2025-09-11 14:21:38 -0400

    Switch to a more active aerials github project

--- dot_local/bin/live-wallpapers.sh
+++ dot_local/bin/live-wallpapers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# pre-load live wallpapers
-
-git clone https://github.com/mikeswanson/wallget.git
-sudo python3 wallget/wallget.py
+# pre-load live wallpapers:
+# /Library/Application Support/com.apple.idleassetsd/Customer/4KSDR240FPS/
+git clone https://github.com/lejacobroy/aerials-downloader.git
+cd aerials-downloader
+/usr/bin/pip3 install -r requirements.txt
+sudo /usr/bin/python3 app.py
