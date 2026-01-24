1730845f36c58db8825671d883b6b7b016432fa7
Chris Hemp <chris@chrishemp.com>
2026-01-24 11:40:07 -0500

    Separate install packages script into smaller ones

---
+++ .chezmoiscripts/darwin/run_once_after_03_install-vim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# lunarvim:
+# https://www.lunarvim.org/docs/installation
+# Have git, make, pip, python, npm, node, cargo and ripgrep installed on your system.
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
