9e603a3565b856b34b7d8fc87409ca85a164e72c
Chris Hemp <chris@chrishemp.com>
2026-02-01 17:21:16 -0500

    Add script for quick reminders where to clickops off all this telemetry

---
+++ dot_local/bin/macos-telemetry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ClickOps for 2026-01 - MacOS Tahoe - much telemetry such privacy
+
+# Disable everything but Applications.
+open "x-apple.systempreferences:com.apple.Spotlight-Settings.extension"
+
+# Disable Siri
+open "x-apple.systempreferences:com.apple.Siri-Settings.extension"
