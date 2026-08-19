b5bae34963a545f26f09169346785c0fc65363f2
Chris Hemp <chris@chrishemp.com>
2026-08-19 10:30:31 -0400

    reorganize zprofile

--- dot_oh-my-zsh/custom/00_dotfiles.zsh
+++ dot_oh-my-zsh/custom/00_dotfiles.zsh
@@ -11,47 +11,11 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 
-export PATH="$HOME/.local/bin:$PATH"
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-
-# Disable Telemetry
-# "universal" signals (honored by some, ignored by many)
-export DISABLE_TELEMETRY=1
-export DO_NOT_TRACK=1
-export NO_TELEMETRY=1
-
-# Tools that need their own specific var
-export ASTRO_TELEMETRY_DISABLED=1          # Astro
-export AZURE_CORE_COLLECT_TELEMETRY=0      # Azure CLI
-export CDK_DISABLE_CLI_TELEMETRY=true      # AWS
-export CHECKPOINT_DISABLE=1                # Prisma
-export DOTNET_CLI_TELEMETRY_OPTOUT=1       # .NET CLI
-export EXPO_NO_TELEMETRY=1                 # Expo
-export GATSBY_TELEMETRY_DISABLED=1         # Gatsby
-export GEMINI_TELEMETRY_ENABLED=false      # Gemini CLI
-export GH_TELEMETRY=false                  # GitHub CLI
-export HOMEBREW_NO_ANALYTICS=1             # Homebrew
-export NEXT_TELEMETRY_DISABLED=1           # Next.js
-export NUXT_TELEMETRY_DISABLED=1           # Nuxt
-export SAM_CLI_TELEMETRY=0                 # AWS SAM CLI
-export STORYBOOK_DISABLE_TELEMETRY=1       # Storybook
-export VERCEL_PLUGIN_TELEMETRY=off         # Vercel
-export VSCODE_CRASH_REPORTER_DISABLE=1     # VS Code
-export VSCODE_TELEMETRY_DISABLE=1          # VS Code
-export WRANGLER_SEND_METRICS=false         # Cloudflare Wrangler
-
 # Avoid issues with `gpg` (installed via Homebrew)
 # Ref: https://stackoverflow.com/a/42265848/96656
-export GPG_TTY=$TTY
-
-# https://github.com/junegunn/fzf?tab=readme-ov-file#environment-variables
-export FZF_DEFAULT_COMMAND="fd"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-
-export FZF_CTRL_T_OPTS="
-  --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+if [[ -o interactive && -n "$TTY" ]]; then
+  export GPG_TTY="$TTY"
+fi
 
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -95,6 +59,4 @@ _updateitermcolorschemes() (
   git -C "$HOME/.iterm2-color-schemes" pull
 )
 
-command -v brew >/dev/null || export PATH="/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:$PATH"
-command -v brew >/dev/null && eval "$(brew shellenv)"
 eval "$(mise activate zsh)"
